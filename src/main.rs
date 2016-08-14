4468b7315cdfee538d0511b5faee73e2c8225cc0
Alexander Thaller <alexander.thaller@trivago.com>
2016-08-14 17:01:44 +0200

    Added logging.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,54 @@
 extern crate iron;
+#[macro_use]
+extern crate hyper;
+#[macro_use]
+extern crate logger;
 
 use iron::prelude::*;
 use iron::status;
+use std::net::IpAddr;
+use iron::method;
+use std::str::FromStr;
+use logger::Logger;
+use logger::format::Format;
+
+header! { (XForwardedFor, "X-Forwarded-For") => [String] }
 
 fn main() {
-    fn hello_world(r: &mut Request) -> IronResult<Response> {
-        println!("Remote addr is {}", r.remote_addr.ip());
-        Ok(Response::with((status::Ok, format!("{}", r.remote_addr.ip()))))
+    let format = Format::new("@[bold]{method}@ {uri} @[bold]->@ @[C]{ip-addr}@ -- @[C]{status}@ ({response-time})",
+                             vec![],
+                             vec![]);
+
+    let (logger_before, logger_after) = Logger::new(Some(format.unwrap()));
+
+    let mut chain = Chain::new(root);
+
+    // Link logger_before as your first before middleware.
+    chain.link_before(logger_before);
+
+    // Link logger_after as your *last* after middleware.
+    chain.link_after(logger_after);
+
+    let binding = "127.0.0.1:8080";
+    Iron::new(chain).http(binding).unwrap();
+}
+
+fn remote_or_xforwarded_for(req: &Request) -> IpAddr {
+    if req.headers.has::<XForwardedFor>() {
+        let ip_raw = req.headers.get::<XForwardedFor>().unwrap().as_str();
+        return IpAddr::from_str(ip_raw).unwrap();
     }
 
-    println!("On 3000");
-    Iron::new(hello_world).http("localhost:3000").unwrap();
+    req.remote_addr.ip()
+}
+
+fn root(req: &mut Request) -> IronResult<Response> {
+    Ok(match req.method {
+        method::Get => {
+            let ip = remote_or_xforwarded_for(req);
+            req.remote_addr.set_ip(ip);
+            Response::with((status::Ok, format!("{}", ip)))
+        }
+        _ => Response::with(status::BadRequest),
+    })
 }
