1181f9adfdc0aae255281c0f07196d76c94733c6
Alexander Thaller <alexander.thaller@trivago.com>
2018-05-20 19:30:46 +0200

    Switched to rocket web framework.

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,46 @@
-extern crate iron;
-#[macro_use]
-extern crate hyper;
-extern crate logger;
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
 
-use iron::method;
-use iron::prelude::*;
-use iron::status;
-use logger::format::Format;
-use logger::Logger;
-use std::net::IpAddr;
-use std::str::FromStr;
+extern crate rocket;
 
-header! { (XForwardedFor, "X-Forwarded-For") => [String] }
+use rocket::request::{self, FromRequest};
+use rocket::{Outcome, Request};
+
+#[get("/")]
+fn index<'a>(req: RemoteAddr) -> String {
+    req.addr()
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index])
+}
 
 fn main() {
-    let format = Format::new(
-        "@[bold]{method}@ {uri} @[bold]->@ @[C]{ip-addr}@ -- @[C]{status}@ \
-         ({response-time})",
-        vec![],
-        vec![],
-    );
-
-    let (logger_before, logger_after) = Logger::new(Some(format.unwrap()));
-
-    let mut chain = Chain::new(root);
-
-    // Link logger_before as your first before middleware.
-    chain.link_before(logger_before);
-
-    // Link logger_after as your *last* after middleware.
-    chain.link_after(logger_after);
-
-    let binding = "0.0.0.0:8080";
-    Iron::new(chain).http(binding).unwrap();
+    rocket().launch();
 }
 
-fn remote_or_xforwarded_for(req: &Request) -> IpAddr {
-    if req.headers.has::<XForwardedFor>() {
-        let ip_raw = req.headers.get::<XForwardedFor>().unwrap().as_str();
-        return IpAddr::from_str(ip_raw).unwrap();
+struct RemoteAddr {
+    addr: String,
+}
+
+impl RemoteAddr {
+    fn addr(self) -> String {
+        self.addr
     }
-
-    req.remote_addr.ip()
 }
 
-fn root(req: &mut Request) -> IronResult<Response> {
-    Ok(match req.method {
-        method::Get => {
-            let ip = remote_or_xforwarded_for(req);
-            req.remote_addr.set_ip(ip);
-            Response::with((status::Ok, format!("{}", ip)))
+impl<'a, 'r> FromRequest<'a, 'r> for RemoteAddr {
+    type Error = ();
+
+    fn from_request(req: &'a Request<'r>) -> request::Outcome<Self, ()> {
+        if req.headers().contains("X-Forwarded-For") {
+            Outcome::Success(RemoteAddr {
+                addr: format!("{}", req.headers().get_one("X-Forwarded-For").unwrap()),
+            })
+        } else {
+            Outcome::Success(RemoteAddr {
+                addr: format!("{}", req.remote().unwrap()),
+            })
         }
-        _ => Response::with(status::BadRequest),
-    })
+    }
 }
